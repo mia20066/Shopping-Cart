16f8e4b5f91425f7c729884e9dd3cf5e89567b3a
mia <dimamia1994@gmail.com>
2023-04-06 21:28:55 -0400

    update-2

---
+++ src/components/reducers/cartReducer.js
@@ -0,0 +1,117 @@
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+
+const initState = {
+    items: [
+        {id:1,title:'Chicken Dinner', desc: "Marinated Chicken breast cubes, served on a bed of rice, with a side of tomato sauce Served with pita and a side house salad.", price:20.95,img:Item1},
+        {id:2,title:'Kafta Dinner', desc: "Ground Sirloin, mixed with parsley, onions, and spices. Served on a bed of rice with a side of tomato sauce. Served with pita and a side house salad.", price:20.99,img: Item2},
+        {id:3,title:'Gyro Dinner', desc: "Grilled Gyro meat, served on a bed of rice, with a side of tomato sauce. Served with pita and a side house salad.",price:120,img: Item3},
+        {id:4,title:'Lamb Shank', desc: "Fork tender Lamb Shank, slightly drizzled in tomato sauce, served on a bed of rice, with a side of tomato sauce. Served with pita and a side house salad.", price:19.95,img:Item4},
+        {id:5,title:'Falafel Dinner', desc: "Six freshly fried Falafel balls, served on a bed of rice, with a side of tahini sauce. Served with pita and a side house salad.", price:19.95,img: Item5},
+        {id:6,title:'Baked Kibbi Dinner', desc: "Mixture of Beef, cracked wheat, and herbs. Baked and stuffed with sautéed Beef, onions and pine nuts. Served with pita and a side house salad.",price:17.95,img: Item6}
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 6
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 6
+        }
+  }
+    
+  else{
+    return state
+    }
+    
+}
+
+export default cartReducer
